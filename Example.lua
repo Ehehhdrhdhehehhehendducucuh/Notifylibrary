49c8cf92f2f899ce1efdf36228a145f2088ac6b9
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-07 09:30:45 +0800

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,3 @@
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Notifylibrary/refs/heads/main/notifylibrary.lua.txt"))() -- library
+
+Notification:Notify("What the text will send?", "Tests", 5) -- notify
